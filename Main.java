f78cfc37fea3fd9e84cd4d45ca4450cda7097b54
JordanHal <36819808+JordanHal@users.noreply.github.com>
2018-03-07 13:32:07 -0300

    Update Main.java (Questão 8)

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 class Main
 {
-   public static void main(String args[])
-   {  
-      Pessoa p1 = new Pessoa();
-      p1.setNome("Ana");
-      p1.setIdade(18);
-      Pessoa p2 = new Pessoa();
-      p2.setNome("João");
-      p1.setIdade(15);
-      System.out.println("Nome1:"+p1.getNome()+"\nIdade:"+p1.getIdade());
-      System.out.println(p2.getNome()+p2.getIdade());
-      
-   }
+	public static void main ( String args[])
+	{
+		Pessoa p1 = new Pessoa ("Ana", 18);		
+		Pessoa p2 = new Pessoa("Joao", 15);
+
+		
+
+		System.out.println("Nome: "+p1.getNome()+"\nidade: "+p1.getIdade()+"\n ");
+		System.out.println("Nome: "+p2.getNome()+"\nIdade: "+p2.getIdade());
+	}
 }
